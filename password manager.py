0345b1880e237c1ce65b7f85830ac9b9d35725a7
TGvenomYT <rajicbe1969@gmail.com>
2024-12-05 22:15:18 +0530

    Update password manager.py

--- password manager.py
+++ password manager.py
@@ -1,26 +1,56 @@
-
-
 from cryptography.fernet import Fernet
-key=Fernet.generate_key()
+
+def write_key():
+    file=open('key.key','wb')
+    key = Fernet.generate_key()
+    file.write(key)
+    file.close()
+
+
+def read_key():
+    file=open('key.key','rb')
+    key=file.read()
+    print('returned key=')
+    return key
+    file.close()
+
+
+
+key=read_key()
 fer=Fernet(key)
 
 
-def add():
-    e=open('text.txt','a')
-    acc=input('enter account name:')
-    pwd=input('enter password:')
-    encrypted_file=fer.encrypt(pwd.encode()).decode()
-    e.write(acc +"|" +encrypted_file+ "\n")
-    e.close()
-
 
 def view():
-     e=open('text.txt','r')
-     for i in e.readlines():
-         data=i.rstrip()
-         acc,pwd=data.split('|')
-         decrypted_token=fer.decrypt(pwd.encode()).decode()
-         print(acc+'|'+decrypted_token)
-         e.close()
-add()
-view()
+      with open('text.txt','r')as f:
+         for lines in f.readlines():
+           data=lines.rstrip()
+           uname,pwd=data.split('|')
+           decrypted_pwd=fer.decrypt(pwd.encode()).decode()
+           print(uname,'|',decrypted_pwd)
+
+
+
+def add():
+    uname=str(input('enter username'))
+    pwd = str(input('enter password'))
+    with open('text.txt', 'a') as f:
+          encrypted_file = fer.encrypt(pwd.encode()).decode()
+          f.write(uname + '|' + encrypted_file + '\n')
+
+
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
